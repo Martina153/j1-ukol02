a65d757a251b259ebd9890c0428d14784ec587fb
Filip Jirsák <filip@jirsak.org>
2024-01-28 21:19:17 +0100

    Aktualizace jaro 2024 – Java 21, Gradle 8.5, externí knihovna turtle-engine.

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -1,12 +1,12 @@
 package cz.czechitas.turtle;
 
-import cz.czechitas.turtle.engine.*;
+import dev.czechitas.java1.turtle.engine.*;
 
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
     public void start() {
-
+        //TODO Tady bude kód pro kreslení želví grafiky.
     }
 
     public static void main(String[] args) {
