9af317954c611f58b9b240cc6c74b95279bd9030
Filip Jirsák <filip@jirsak.org>
2024-01-29 22:00:45 +0100

    Oprava formátování kódu dle výchozího nastavení InetlliJ Idea.

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -3,14 +3,14 @@ package cz.czechitas.turtle;
 import dev.czechitas.java1.turtle.engine.Turtle;
 
 public class HlavniProgram {
-  private Turtle zofka = new Turtle();
+    private Turtle zofka = new Turtle();
 
-  public static void main(String[] args) {
-    new HlavniProgram().start();
-  }
+    public static void main(String[] args) {
+        new HlavniProgram().start();
+    }
 
-  public void start() {
-    //TODO Tady bude kód pro kreslení želví grafiky.
-  }
+    public void start() {
+        //TODO Tady bude kód pro kreslení želví grafiky.
+    }
 
 }
